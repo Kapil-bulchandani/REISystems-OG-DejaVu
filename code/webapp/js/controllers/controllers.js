bca4bf83f3880466860a4588a39a61454f2c10c3
hieu-trang-reisys <hieu.trang@reisystems.com>
2015-09-09 15:12:52 -0400

    Framework for querying mongodb for data

--- code/webapp/js/controllers/controllers.js
+++ code/webapp/js/controllers/controllers.js
@@ -2,23 +2,17 @@ var dejaVuControllers = angular.module('dejaVuControllers', []);
 
 dejaVuControllers.controller('FormListController', ['$scope', 'DejaVuForm',
     function($scope, DejaVuForm) {
-        $scope.forms = DejaVuForm.query();
-
+        //$scope.forms = DejaVuForm.query();
         //  TODO: Testing and development purposes. Remove once database is populated
-        if (!$scope.forms) {
-            $scope.forms = [{formId: "1", form: "i-130", applicant: {"name": "Viktor Pylypenko"}}, {formId: "2", form: "i-140", applicant: {"name": "John Doe"}}, {formId: "3", form: "i-131", applicant: {"name": "Bruce Wayne"}}];
-        }
+        $scope.forms = [{formId: "1", form: "i-130", applicant: {"name": "Viktor Pylypenko"}}, {formId: "2", form: "i-140", applicant: {"name": "John Doe"}}, {formId: "3", form: "i-131", applicant: {"name": "Bruce Wayne"}}];
     }
 ]);
 
 dejaVuControllers.controller('FormDetailController', ['$scope', '$routeParams', 'DejaVuForm',
     function($scope, $routeParams, DejaVuForm) {
-        $scope.currentForm = DejaVuForm.get({formId: $routeParams.formId});
-
+        //$scope.currentForm = DejaVuForm.get({formId: $routeParams.formId});
         //  TODO: Testing and development purposes. Remove once database is populated
-        if (!$scope.currentForm) {
-            $scope.currentForm = {formId: "1", form: "i-130", applicant: {"name": "Viktor Pylypenko"}};
-        }
+        $scope.currentForm = {formId: "1", form: "i-130", applicant: {"name": "Viktor Pylypenko"}};
     }
 ]);
 
@@ -29,6 +23,6 @@ dejaVuControllers.controller('FormTemplateController', ['$scope',
             'i-131': 'js/partials/forms/i-131.html',
             'i-140': 'js/partials/forms/i-140.html'
         };
-        $scope.template = $scope.templates[$scope.currenForm.form];
+        $scope.template = $scope.templates[$scope.currentForm.form];
     }
 ]);
--- code/webapp/js/utilities/services.js
+++ code/webapp/js/utilities/services.js
@@ -2,8 +2,7 @@ var dejaVuServices = angular.module('dejaVuServices', ['ngResource']);
 
 dejaVuServices.factory('DejaVuForm', ['$resource',
     function($resource){
-        var $baseUrl = 'ec2-52-2-165-143.compute-1.amazonaws.com:28017/dejavu_mongo/';
-        return $resource($baseUrl, {}, {
+        return $resource('http://ec2-52-2-165-143.compute-1.amazonaws.com:28017/dejavu_mongo/', {}, {
             query: {method:'GET', params: {'filter_': '@formId'}, isArray:true},
             get: {method:'GET', params: {'filter_': '@formId'}, isArray:false}
         });
